ebbb297a814407352ce6267100868a4107fec720
Elizabeth <elizabethyxbrown@gmail.com>
2025-02-23 17:49:19 +0000

    testing tutorial done

--- src/student/dash_single/paralympics_dash.py
+++ src/student/dash_single/paralympics_dash.py
@@ -40,7 +40,7 @@ row_two = dbc.Row([
                 {"label": "Winter", "value": "winter"},
             ],
             value=["summer"],  # Values is a list as you can select 1 AND 2
-            id="checklist-input",
+            id="checklist-games-type",
         ),
     ]
 )], width={"size": 4, "offset": 2}),
@@ -49,30 +49,30 @@ row_two = dbc.Row([
 
 row_three = dbc.Row([
     dbc.Col(children=[dcc.Graph(id="line-chart", figure=line_chart("sports")),], width=6),
-    dbc.Col(children=[html.Div(children=[], id="bar-graph")], width=6)
+    dbc.Col(children=[], id='bar-div', width=6),
 ])
 
 row_four = dbc.Row([
     dbc.Col(children=[dcc.Graph(id="map-fig", figure=scatter_geo())], width=8),
-    dbc.Col(children=[dbc.Card([
-    dbc.CardImg(src=app.get_asset_url("logos/2022_Beijing.jpg"), top=True),
-    dbc.CardBody([
-        html.H4("Beijing 2022", className="card-title"),
-        html.P("Number of athletes: XX", className="card-text", ),
-        html.P("Number of events: XX", className="card-text", ),
-        html.P("Number of countries: XX", className="card-text", ),
-        html.P("Number of sports: XX", className="card-text", ),
-    ]),
-],
-    style={"width": "18rem"},
-    id="map-card")  # Added id here
-], width=4)
+#     dbc.Col(children=[dbc.Card([
+#     dbc.CardImg(src=app.get_asset_url("logos/2022_Beijing.jpg"), top=True),
+#     dbc.CardBody([
+#         html.H4("Beijing 2022", className="card-title"),
+#         html.P("Number of athletes: XX", className="card-text", ),
+#         html.P("Number of events: XX", className="card-text", ),
+#         html.P("Number of countries: XX", className="card-text", ),
+#         html.P("Number of sports: XX", className="card-text", ),
+#     ]),
+# ],
+#     style={"width": "18rem"},
+#     id="map-card")  # Added id here
+# ], width=4)
 ])
 
 
 row_five = dbc.Row([
     dbc.Col(children=[dcc.Graph(id="go_map", figure=go_map())], width=8),
-    dbc.Col(children=[card_fig("Barcelona 1992", app)], id="card", width=4),
+    dbc.Col(children=[], id="card", width=4),
     ])
 
 row_six = dbc.Row([
@@ -81,7 +81,7 @@ row_six = dbc.Row([
 
  #Callback for map
 @app.callback(
-    Output(component_id='map-card', component_property='children'),
+    Output(component_id='card', component_property='children'),
     Input(component_id='map-fig', component_property='hoverData')
 )
 
@@ -93,12 +93,23 @@ def display_card(hover_data):
 
 # Callback for bar chart and dropdown 
 @app.callback(
-    Output(component_id='bar-graph', component_property='children'),
-    Input(component_id='checklist-input', component_property='value')
+    Output(component_id='bar-div', component_property='children'),
+    Input(component_id='checklist-games-type', component_property='value')
 )
-def update_bar_chart(features):
-    graphs = [html.Div(dcc.Graph(figure=bar_gender(feature), id=f"bar-graph-{feature}"), id="bar-div") for feature in features]
-    return graphs  # Returning a list of components works in Dash
+def update_bar_chart(selected_values):
+    """ Updates the bar chart based on the checklist selection.
+     Creates one chart for each of the selected values.
+     """
+    figures = []
+    # Iterate the list of values from the checkbox component
+    for value in selected_values:
+        fig = bar_gender(value)
+        # Assign id to be used to identify the charts
+        id = f"bar-chart-{value}"
+        element = dcc.Graph(figure=fig, id=id)
+        figures.append(element)
+    return figures
+
 
 # Callback for line chart
 @app.callback(
--- tests/test_paralympics_dash.py
+++ tests/test_paralympics_dash.py
@@ -1,6 +1,9 @@
 import requests
 from dash.testing.application_runners import import_app
-
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.support.wait import WebDriverWait
+from selenium.webdriver import ActionChains
 
 def test_server_live(dash_duo):
     """
@@ -43,7 +46,7 @@ def test_home_h1_text_equals(dash_duo):
     h1_text = dash_duo.find_element("h1").text  # Dash function version
 
     # Assertion checks that the heading has the expected text
-    assert h1_text == "Paralympics Dashboard"
+    assert h1_text == "Paralympics Data Analytics"
 
 def test_dropdown_values(dash_duo):
     """
@@ -73,8 +76,7 @@ def test_dropdown_values(dash_duo):
 def test_bar_chart_updates(dash_duo):
     """
     GIVEN the app is running
-    AND the checkboxes are present on the page
-    AND a bar chart is present on the page
+    AND the checkboxes are available on the page
     WHEN both checkboxes are selected
     THEN the two charts should be in the div with the id 'bar-div' (can be tested with the class=dash-graph)
     """
@@ -83,40 +85,110 @@ def test_bar_chart_updates(dash_duo):
     dash_duo.start_server(app)
 
     # Wait until the checkbox element is displayed
-    dash_duo.wait_for_element("#checklist-input", timeout=4)
-
-    # Find div with the id 'bar-div' that contains the charts
-    bar_div = dash_duo.find_element("#bar-graph")
-
-    # count the number of elements in bar_div with the class 'dash-graph'
-    graphs_in_bar_div = bar_div.find_elements_by_class_name("dash-graph")
-    
-    num_charts_before = len(bar_div.find_elements_by_id("bar-div"))
+    dash_duo.wait_for_element_by_id("checklist-games-type", timeout=2)
 
     # Select the 'Winter' checkbox. Summer is already selected by default.
-    
+    winter_checkbox = dash_duo.find_element("#checklist-games-type input[value='winter']")
+    # Click the checkbox
+    winter_checkbox.click()
 
-    checklist = dash_duo.find_element("#checklist-input")
+    # Wait for the bar chart elements to be present
+    dash_duo.wait_for_element_by_id("bar-chart-winter", timeout=10)
+    dash_duo.wait_for_element_by_id("bar-chart-summer", timeout=3)
 
-    # Find all checkboxes inside the checklist
-    checkboxes = checklist.find_elements_by_tag_name("input")
-
-    # Click the second checkbox (Winter)
-    checkboxes[1].click()
-
-
-    # Wait 2 seconds for the bar chart to update
-    dash_duo.driver.implicitly_wait(2)
-
-    # Find div with the id 'bar-div' that contains the charts again
-    bar_div = dash_duo.find_element("#bar-graph")
-
+    # Find div with the id 'bar-div' that contains the charts
+    bar_div = dash_duo.find_element("#bar-div")
     # count the number of elements in bar_div with the class 'dash-graph'
-    graphs_in_bar_div = bar_div.find_elements_by_id("bar-div")
-    num_charts_after = len(graphs_in_bar_div)
-    
-    # There should be 2 charts now and 1 at the start so you can assert that num_charts_after > num_charts_before
-    assert num_charts_after > num_charts_before
+    # This is a list of elements that have the class 'dash-graph'
+    # The length of this list is the number of elements with that class
+    num_charts = len(bar_div.find_elements_by_class_name("dash-graph"))
+    # Alternatively, use ID selector to find 'bar-chart-winter' and 'bar-chart-summer'
+    winter_chart = len(bar_div.find_elements_by_id("bar-chart-winter"))
+    summer_chart = len(bar_div.find_elements_by_id("bar-chart-summer"))
+
+    # There should be 2 charts
+    assert winter_chart == 1
+    assert summer_chart == 1
+    assert num_charts == 2
 
 
+def test_line_chart_changes(dash_duo):
+    """
+    GIVEN the app is running
+    WHEN the dropdown is used to select "Sports"
+    THEN the line chart should be displayed
+    AND the chart title should include the word "sports"
+    """
 
+    # Start the app in a server
+    app = import_app(app_file="student.dash_single.paralympics_dash")
+    dash_duo.start_server(app)
+
+    # Find the title displayed in the plotly express chart
+    # The .gtitle class name was found using the browser developer tools to inspect the element
+    dash_duo.wait_for_element_by_id("line-chart", timeout=2)
+    line_chart_title_start = dash_duo.find_element("#line-chart .gtitle").text
+
+    # This uses a Dash testing method to find the element by its id, not the selenium method
+    dash_duo.wait_for_element_by_id("dropdown-category", timeout=2)
+
+    # Find the dropdown element by its id and select the option with the text "Sports"
+    # see https://www.testim.io/blog/selenium-select-dropdown scroll down to the Python example
+    sport_opt_xpath = "//select[@id='dropdown-category']/option[text()='Sports']"
+    dash_duo.driver.find_element(by=By.XPATH, value=sport_opt_xpath).click()
+
+    # Wait for the #line-chart element to be present and updated
+    # dash_duo.wait_for_element_by_id("line-chart", timeout=2)
+
+    WebDriverWait(dash_duo.driver, 3).until(
+        EC.text_to_be_present_in_element((By.CSS_SELECTOR, '#line-chart .gtitle'), 'sports')
+    )
+
+    # Find the title displayed in the plotly express chart
+    chart_title_end = dash_duo.find_element("#line-chart .gtitle").text
+
+    # Assert that the title includes the word "sports" and the title has changed
+    assert "sports" in chart_title_end.lower()
+    assert line_chart_title_start != chart_title_end
+
+def test_map_marker_hover_updates_card(dash_duo):
+    """
+    GIVEN the app is running which has a <div id='map>
+    THEN there should not be any elements with a class of 'card' one the page
+    WHEN a marker in the map is selected
+    THEN there should be one more card on the page then there was at the start
+    AND there should be a text value for the h6 heading in the card
+    """
+    # Start the app in a server
+    app = import_app(app_file="student.dash_single.paralympics_dash")
+    dash_duo.start_server(app)
+
+    # Wait for the div with id of card to be on the page
+    dash_duo.wait_for_element("#card", timeout=2)
+
+    # There is no card so finding elements with a bootstrap class of 'card' should have a length of 0
+    cards = dash_duo.find_elements(".card")
+    cards_count_start = len(cards)
+
+    # Find the first map marker
+    marker_selector = '#map-fig > div.js-plotly-plot > div > div > svg:nth-child(1) > g.geolayer > g > g.layer.frontplot > g > g > path:nth-child(1)'
+    marker = dash_duo.driver.find_element(By.CSS_SELECTOR, marker_selector)
+
+    # Use the Actions API and build a chain to move to the marker and hover
+    ActionChains(dash_duo.driver).move_to_element(marker).pause(2).perform()
+
+    # Wait for the element with class of 'card'
+    dash_duo.wait_for_element(".card", timeout=1)
+
+    # Count the cards again
+    cards = dash_duo.find_elements(".card")
+    cards_count_end = len(cards)
+
+    # Find the card title and get the textContent attribute value
+    title_selector = '//*[@id="card"]/div/div/h4'
+    card_title = dash_duo.driver.find_element(by=By.XPATH,value=title_selector)
+    text = card_title.get_attribute("textContent")
+
+    # The card title should have changed and there should be more cards
+    assert text != ""
+    assert cards_count_end > cards_count_start
